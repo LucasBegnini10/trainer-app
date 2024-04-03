1b2f306339a305b270596b4f1076f77f667aa329
LucasBegnini10 <begninilucas12@gmail.com>
2024-04-03 19:47:28 -0300

    feat: screen create exercise

--- src/controllers/trainer/useCreateWorkout.ts
+++ src/controllers/trainer/useCreateWorkout.ts
@@ -1,13 +1,86 @@
+import { useState } from "react";
 import useExerciseList from "./useExerciseList";
 import useStudents from "./useStudents";
+import { ExercisesModel, UserModel, WorkoutModel } from "../../models/models";
+import { daysOfWeekMapping } from "../../utils/schedule";
 
 export default function useCreateWorkout() {
   const { students } = useStudents();
   const { exercises } = useExerciseList();
 
+  const [workout, setWorkout] = useState({} as WorkoutModel);
+  const [studentsSelected, setStudentsSelected] = useState(
+    [] as Array<UserModel>
+  );
+  const [exercisesSelected, setExercisesSelected] = useState(
+    [] as Array<ExercisesModel>
+  );
+
+  const daysOfWeekOptions = Object.entries(daysOfWeekMapping);
+
+  const toggleOptionDayOfWeek = (day: string) => {
+    const dayInt = parseInt(day);
+    setWorkout((prev) => {
+      const scheduleDescription = prev.schedule_description || [];
+
+      if (scheduleDescription.includes(dayInt)) {
+        return {
+          ...prev,
+          schedule_description: scheduleDescription.filter(
+            (item) => item !== dayInt
+          ),
+        };
+      }
+
+      return {
+        ...prev,
+        schedule_description: [...scheduleDescription, dayInt],
+      };
+    });
+  };
+
+  const handleSetStudentSelected = (student: UserModel) => {
+    setStudentsSelected((prev) => [...new Set([...prev, student])]);
+  };
+
+  const handleRemoveStudentSelected = (student: UserModel) => {
+    setStudentsSelected((prev) =>
+      prev.filter((item) => item.id !== student.id)
+    );
+  };
+
+  const handleSetExerciseSelected = (exercise: ExercisesModel) => {
+    setExercisesSelected((prev) => [...new Set([...prev, exercise])]);
+  };
+
+  const handleRemoveExerciseSelected = (exercise: ExercisesModel) => {
+    setExercisesSelected((prev) =>
+      prev.filter((item) => item.id !== exercise.id)
+    );
+  };
+
   return {
     students,
-    exercises
+    exercises,
+    workout: {
+      get: workout,
+      set: setWorkout,
+    },
+    studentsSelected: {
+      get: studentsSelected,
+      set: handleSetStudentSelected,
+      remove: handleRemoveStudentSelected,
+    },
+    exercisesSelected: {
+      get: exercisesSelected,
+      set: handleSetExerciseSelected,
+      remove: handleRemoveExerciseSelected,
+    },
+    scheduleDescription: {
+      options: daysOfWeekOptions,
+      toggle: toggleOptionDayOfWeek,
+      state: workout.schedule_description,
+    },
   };
 }
 
--- src/utils/string.ts
+++ src/utils/string.ts
@@ -2,9 +2,9 @@ export const getInitials = (text: string) => {
   const words = text.split(" ");
 
   const first = words[0];
-  const last = words[words.length - 1];
+  const last = words?.[words.length - 1];
 
-  return `${first[0]}${last[0]}`;
+  return `${first?.[0]}${last?.[0] || first?.[1]}`;
 };
 
 export const onlyNumbers = (str: string): string =>
