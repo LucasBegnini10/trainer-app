fb2d24b5e582fc1a4f2381747af8d13f12565653
LucasBegnini10 <begninilucas12@gmail.com>
2024-04-28 16:30:07 -0300

    fix: add keyboardavoidview in register and login page

--- app/(login)/login.tsx
+++ app/(login)/login.tsx
@@ -5,14 +5,19 @@ import {
   Button,
   Center,
   Checkbox,
+  Heading,
   Icon,
   IconButton,
   Image,
+  KeyboardAvoidingView,
   Row,
+  ScrollView,
   Text,
   View,
 } from "native-base";
 import { Ionicons } from "@expo/vector-icons";
+import { Keyboard, Platform, TouchableWithoutFeedback } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function LoginPage() {
   const {
@@ -29,91 +34,110 @@ export default function LoginPage() {
     loading,
   } = useLogin();
 
+  const inset = useSafeAreaInsets();
+
   return (
-    <View
-      flex={1}
-      bg="brand.bg"
-      justifyContent={"center"}
-      px={4}
-      style={{ gap: 20 }}
+    <KeyboardAvoidingView
+      style={{
+        flex: 1,
+        paddingTop: inset.top,
+      }}
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      enabled
+      bg={"brand.bg"}
     >
-      <Center mb={4}>
-        <Image
-          alt="Logo"
-          source={require("../../assets/logo.png")}
-          w={32}
-          h={32}
-        />
-      </Center>
-      <Text color="brand.primary" fontFamily={"Roboto-Bold"} fontSize={22}>
-        Bem vindo(a) de volta
-      </Text>
-      <InputComponent
-        label="Digite seu e-mail"
-        onChange={setEmail}
-        value={email}
-        invalid={invalid.email.invalid}
-        error={invalid.email.error}
-        inputProps={{
-          autoCapitalize: "none",
-          textContentType: "emailAddress",
-        }}
-        placeholder="E-mail"
-      />
-      <InputComponent
-        invalid={invalid.password.invalid}
-        error={invalid.password.error}
-        label="Digite sua senha"
-        onChange={setPassword}
-        value={password}
-        secure={!seePassword}
-        placeholder="Senha"
-        inputProps={{ autoCapitalize: "none" }}
-        icon={
-          <IconButton
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <ScrollView
+          flexGrow={1}
+          px={4}
+          contentContainerStyle={{ gap: 20, justifyContent: "center", marginVertical: "auto"}}
+        >
+          <Center mb={4}>
+            <Image
+              alt="Logo"
+              source={require("../../assets/logo.png")}
+              w={32}
+              h={32}
+            />
+          </Center>
+          <Heading
+            color="brand.primary"
+            fontFamily={"Roboto-Bold"}
+            fontSize={22}
+          >
+            Bem vindo(a) de volta
+          </Heading>
+          <InputComponent
+            label="Digite seu e-mail"
+            onChange={setEmail}
+            value={email}
+            invalid={invalid.email.invalid}
+            error={invalid.email.error}
+            inputProps={{
+              autoCapitalize: "none",
+              textContentType: "emailAddress",
+            }}
+            placeholder="E-mail"
+          />
+          <InputComponent
+            invalid={invalid.password.invalid}
+            error={invalid.password.error}
+            label="Digite sua senha"
+            onChange={setPassword}
+            value={password}
+            secure={!seePassword}
+            placeholder="Senha"
+            inputProps={{ autoCapitalize: "none" }}
             icon={
-              <Icon
-                color={"white"}
-                as={Ionicons}
-                name={seePassword ? "eye" : "eye-off"}
+              <IconButton
+                icon={
+                  <Icon
+                    color={"white"}
+                    as={Ionicons}
+                    name={seePassword ? "eye" : "eye-off"}
+                  />
+                }
+                borderRadius="full"
+                onPress={toogleSeePassword}
               />
             }
-            borderRadius="full"
-            onPress={toogleSeePassword}
           />
-        }
-      />
-      <Row alignItems={"center"} justifyContent={"space-between"}>
-        <Row space={2}>
-          <Checkbox
-            borderColor={"brand.bg"}
-            _checked={{ bg: "brand.primary", borderColor: "brand.primary" }}
-            value={saveData ? "false" : "true"}
-            _pressed={{backgroundColor: "brand.secondary"}}
-            onChange={setSaveData}
-          >
-            <Text color={"white"}>Salvar dados</Text>
-          </Checkbox>
-        </Row>
-        <View>
-          <Link href={"/forgot-password"}>
-            <Text color={"white"}>Esqueci minha senha</Text>
-          </Link>
-        </View>
-      </Row>
-      <Button mt={4} onPress={handleLogin} isLoading={loading}>
-        <Text fontFamily={"Roboto-Bold"} color={"brand.bg"} fontSize={14}>
-          LOGIN
-        </Text>
-      </Button>
-      <Center>
-        <Text color="white">
-          Ainda não possui conta?{" "}
-          <Link href={"/signup"}>
-            <Text color="brand.primary">Cadastre-se</Text>
-          </Link>
-        </Text>
-      </Center>
-    </View>
+          <Row alignItems={"center"} justifyContent={"space-between"}>
+            <Row space={2}>
+              <Checkbox
+                borderColor={"brand.bg"}
+                _checked={{
+                  bg: "brand.primary",
+                  borderColor: "brand.primary",
+                }}
+                value={saveData ? "false" : "true"}
+                _pressed={{ backgroundColor: "brand.secondary" }}
+                onChange={setSaveData}
+              >
+                <Text color={"white"}>Salvar dados</Text>
+              </Checkbox>
+            </Row>
+            <View>
+              <Link href={"/forgot-password"}>
+                <Text color={"white"}>Esqueci minha senha</Text>
+              </Link>
+            </View>
+          </Row>
+          <Button mt={4} onPress={handleLogin} isLoading={loading}>
+            <Text fontFamily={"Roboto-Bold"} color={"brand.bg"} fontSize={14}>
+              LOGIN
+            </Text>
+          </Button>
+          <Center>
+            <Text color="white">
+              Ainda não possui conta?{" "}
+              <Link href={"/signup"}>
+                <Text color="brand.primary">Cadastre-se</Text>
+              </Link>
+            </Text>
+          </Center>
+        </ScrollView>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
--- app/(signup)/signup.tsx
+++ app/(signup)/signup.tsx
@@ -3,14 +3,19 @@ import InputComponent from "../../src/components/common/input/input";
 import {
   Button,
   Center,
+  Heading,
   Icon,
   IconButton,
   Image,
+  KeyboardAvoidingView,
   ScrollView,
   Text,
+  View,
 } from "native-base";
 import { Ionicons } from "@expo/vector-icons";
 import useSignup from "../../src/controllers/signup/useSignup";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { Platform } from "react-native";
 
 export default function SignUpPage() {
   const {
@@ -22,95 +27,102 @@ export default function SignUpPage() {
     loading,
   } = useSignup();
 
-  return (
-    <ScrollView
-      flex={1}
-      bg={"brand.bg"}
-      contentContainerStyle={{
-        justifyContent: "center",
-        gap: 20
-      }}
-      p={4}
-    >
-      <Center mb={4}>
-        <Image
-          alt="Logo"
-          source={require("../../assets/logo.png")}
-          w={32}
-          h={32}
-        />
-      </Center>
-      <Text fontFamily={"Roboto-Bold"} color="brand.primary" fontSize={22}>
-        Bem vindo(a) treinador(a)!
-      </Text>
-      <InputComponent
-        value={get.name}
-        onChange={set.name}
-        invalid={invalid.name.invalid}
-        error={invalid.name.error}
-        label="Nome"
-        inputProps={{
-          autoCapitalize: "words",
-          textContentType: "name",
-        }}
-        placeholder="Nome e Sobrenome"
-      />
-      <InputComponent
-        value={get.email}
-        onChange={set.email}
-        invalid={invalid.email.invalid}
-        error={invalid.email.error}
-        label="E-mail"
-        inputProps={{
-          autoCapitalize: "none",
-          textContentType: "emailAddress",
-        }}
-        placeholder="seuemail@email.com"
-      />
-      <InputComponent
-        value={get.document}
-        onChange={set.document}
-        invalid={invalid.document.invalid}
-        error={invalid.document.error}
-        label="Documento (CPF/CNPJ)"
-        inputProps={{}}
-        placeholder="###.###.###-##"
-      />
+  const inset = useSafeAreaInsets()
 
-      <InputComponent
-        invalid={invalid.password.invalid}
-        error={invalid.password.error}
-        label="Senha"
-        onChange={set.password}
-        value={get.password}
-        secure={!seePassword}
-        placeholder="*************"
-        inputProps={{ autoCapitalize: "none", }}
-        icon={
-          <IconButton
-            icon={<Icon color="brand.primary" as={Ionicons} name={seePassword ? "eye" : "eye-off"} />}
-            borderRadius="full"
-            onPress={toogleSeePassword}
-          />
-        }
-      />
-      <Button
-        onPress={handleSignup}
-        mt={4}
-        isLoading={loading}
+  return (
+    <KeyboardAvoidingView
+      style={{ flex: 1, paddingTop: inset.top}}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      enabled
+      bg={"brand.bg"}
+    >
+      <ScrollView
+        flexGrow={1}
+        px={4}
+        contentContainerStyle={{ gap: 20, justifyContent: "center" }}
       >
-        <Text fontFamily={"Roboto-Bold"} color={"brand.bg"} fontSize={14}>
-          CADASTRAR
-        </Text>
-      </Button>
-      <Center mb={10}>
-        <Text color={"white"}>
-          Já possui uma conta?{" "}
-          <Link href={"/login"}>
-            <Text color="brand.primary">Faça login</Text>
-          </Link>
-        </Text>
-      </Center>
-    </ScrollView>
+        <Center mb={4}>
+          <Image
+            alt="Logo"
+            source={require("../../assets/logo.png")}
+            w={32}
+            h={32}
+          />
+        </Center>
+        <Heading fontFamily={"Roboto-Bold"} color="brand.primary" fontSize={22}>
+          Bem vindo(a) treinador(a)!
+        </Heading>
+        <InputComponent
+          value={get.name}
+          onChange={set.name}
+          invalid={invalid.name.invalid}
+          error={invalid.name.error}
+          label="Nome"
+          inputProps={{
+            autoCapitalize: "words",
+            textContentType: "name",
+          }}
+          placeholder="Nome e Sobrenome"
+        />
+        <InputComponent
+          value={get.email}
+          onChange={set.email}
+          invalid={invalid.email.invalid}
+          error={invalid.email.error}
+          label="E-mail"
+          inputProps={{
+            autoCapitalize: "none",
+            textContentType: "emailAddress",
+          }}
+          placeholder="seuemail@email.com"
+        />
+        <InputComponent
+          value={get.document}
+          onChange={set.document}
+          invalid={invalid.document.invalid}
+          error={invalid.document.error}
+          label="Documento (CPF/CNPJ)"
+          inputProps={{}}
+          placeholder="###.###.###-##"
+        />
+
+        <InputComponent
+          invalid={invalid.password.invalid}
+          error={invalid.password.error}
+          label="Senha"
+          onChange={set.password}
+          value={get.password}
+          secure={!seePassword}
+          placeholder="*************"
+          inputProps={{ autoCapitalize: "none" }}
+          icon={
+            <IconButton
+              icon={
+                <Icon
+                  color="brand.primary"
+                  as={Ionicons}
+                  name={seePassword ? "eye" : "eye-off"}
+                />
+              }
+              borderRadius="full"
+              onPress={toogleSeePassword}
+            />
+          }
+        />
+        <Button onPress={handleSignup} mt={4} isLoading={loading}>
+          <Text fontFamily={"Roboto-Bold"} color={"brand.bg"} fontSize={14}>
+            CADASTRAR
+          </Text>
+        </Button>
+        <Center mb={10}>
+          <Text color={"white"}>
+            Já possui uma conta?{" "}
+            <Link href={"/login"}>
+              <Text color="brand.primary">Faça login</Text>
+            </Link>
+          </Text>
+        </Center>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
