d0349879f10a74487fad6f4daff36a6748b7806f
LucasBegnini10 <begninilucas12@gmail.com>
2024-04-29 19:59:53 -0300

    feat: filter workouts trainer

--- app/trainer/home/index.tsx
+++ app/trainer/home/index.tsx
@@ -18,74 +18,81 @@ import { formatWorkoutToCard } from "../../../src/utils/workout";
 import { WorkoutModel } from "../../../src/models/models";
 
 export default function Workouts() {
-  const { goToCreateWorkout, workouts, isLoading }: UseWorkoutsType =
-    useWorkouts();
+  const {
+    goToCreateWorkout,
+    workouts,
+    isLoading,
+    filter,
+    setFilter,
+  }: UseWorkoutsType = useWorkouts();
 
   return (
-    <FlatList
-      contentContainerStyle={{ paddingBottom: 20 }}
-      bg={"brand.bg"}
-      data={workouts}
-      ItemSeparatorComponent={() => <View py={2} />}
-      keyExtractor={(item, index) => String(item?.id || index)}
-      ListEmptyComponent={
-        <View px={6}>
-          {isLoading ? (
-            <Center>
-              <Spinner color={"brand.primary"}/>
-            </Center>
-          ) : (
-            <Text color={"gray.600"} mt={2}>
-              Nenhum treino encontrado.
-            </Text>
-          )}
+    <View bg={"brand.bg"} flex={1}>
+      <HStack alignItems={"flex-end"} px={6} space={2} my={4} maxH={10}>
+        <View flex={0.8}>
+          <Input
+            fontSize={14}
+            py={3}
+            h={"full"}
+            bg="brand.gray"
+            borderColor={"brand.gray"}
+            color={"white"}
+            placeholder={"Pesquisar treino"}
+            rounded={"lg"}
+            value={filter}
+            onChangeText={setFilter}
+            _focus={{
+              borderColor: "brand.primary",
+              bg: "brand.gray",
+            }}
+            InputLeftElement={
+              <Icon
+                as={<Ionicons name="search" />}
+                size={4}
+                ml="2"
+                color="muted.400"
+              />
+            }
+          />
         </View>
-      }
-      renderItem={({ item: workout }) => {
-        const params = formatWorkoutToCard(workout as WorkoutModel);
-        return <Card key={workout.id} {...params} />;
-      }}
-      ListHeaderComponent={
-        <HStack alignItems={"flex-end"} px={6} space={2} my={4} maxH={10}>
-          <View flex={0.8}>
-            <Input
-              fontSize={14}
-              py={3}
-              h={"full"}
-              bg="brand.gray"
-              borderColor={"brand.gray"}
-              color={"white"}
-              placeholder={"Pesquisar treino"}
-              rounded={"lg"}
-              _focus={{
-                borderColor: "brand.primary",
-                bg: "brand.gray",
-              }}
-              InputLeftElement={
-                <Icon
-                  as={<Ionicons name="search" />}
-                  size={4}
-                  ml="2"
-                  color="muted.400"
-                />
-              }
-            />
-          </View>
 
-          <View flex={0.2}>
-            <IconButton
-              onPress={goToCreateWorkout}
-              rounded={"lg"}
-              icon={
-                <Icon as={Ionicons} name="add" size={22} color={"brand.bg"} />
-              }
-              bg={"brand.primary"}
-              height={"full"}
-              _pressed={{ bg: "brand.secondary" }}
-            />
+        <View flex={0.2}>
+          <IconButton
+            onPress={goToCreateWorkout}
+            rounded={"lg"}
+            icon={
+              <Icon as={Ionicons} name="add" size={22} color={"brand.bg"} />
+            }
+            bg={"brand.primary"}
+            height={"full"}
+            _pressed={{ bg: "brand.secondary" }}
+          />
+        </View>
+      </HStack>
+      <FlatList
+        contentContainerStyle={{ paddingBottom: 20 }}
+        bg={"brand.bg"}
+        data={workouts}
+        ItemSeparatorComponent={() => <View py={2} />}
+        keyExtractor={(item, index) => String(item?.id || index)}
+        ListEmptyComponent={
+          <View px={6}>
+            {isLoading ? (
+              <Center>
+                <Spinner color={"brand.primary"} />
+              </Center>
+            ) : (
+              <Text color={"gray.600"} mt={2}>
+                Nenhum treino encontrado.
+              </Text>
+            )}
           </View>
-        </HStack>
-      }
-    />
+        }
+        renderItem={({ item: workout }) => {
+          const params = formatWorkoutToCard(workout as WorkoutModel);
+          return <Card key={workout.id} {...params} />;
+        }}
+      />
+    </View>
   );
 }
--- src/controllers/trainer/useWorkouts.ts
+++ src/controllers/trainer/useWorkouts.ts
@@ -2,10 +2,13 @@ import { router } from "expo-router";
 import { getWorkoutsQuery } from "../../services/workout/workoutQuery";
 import { useUserStore } from "../../stores/useUserStore";
 import { WorkoutModel } from "../../models/models";
+import { useState } from "react";
 
 export default function useWorkouts() {
   const goToCreateWorkout = () => router.push("/trainer/workout/create");
   const { user } = useUserStore();
+
+  const [filter, setFilter] = useState("")
 
   const { data, isLoading } = getWorkoutsQuery({
     trainer_id: user.Trainers.trainer_id,
@@ -13,10 +16,18 @@ export default function useWorkouts() {
 
   const workouts = (data?.data?.workouts || [])  as Array<WorkoutModel>
 
+  const finalWorkouts = workouts.filter((workout) => {
+    if(filter === "") return true
+    return workout.name.trim().toLowerCase().includes(filter.trim().toLowerCase())
+  })
+
+
   return {
     goToCreateWorkout,
-    workouts,
-    isLoading
+    workouts: finalWorkouts,
+    isLoading,
+    filter,
+    setFilter
   };
 }
 
